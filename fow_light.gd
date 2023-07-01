89980af354b51653be816d55c56de221c1a97ef6
Hudson <dhudsmith@gmail.com>
2023-06-30 21:13:38 -0400

    use group for light occluders

--- fow_light.gd
+++ fow_light.gd
@@ -10,8 +10,8 @@ class_name FogOfWar
 @export var persistent_reveal: bool = true
 ## The light node in the game scene that will reveal fog of war
 @export var light: Light2D
-## Occluders in the game scene
-@export var light_occluders: Node
+## Name of the group of light occluders
+@export var light_occluder_group: String
 ## Scales down the fow texture for more efficient computation
 @export_range(0.05,1.0) var fow_scale_factor := 1.
 
@@ -23,11 +23,13 @@ class_name FogOfWar
 var light_dup : Light2D
 var mask_image: Image
 var mask_texture: Texture2D
+var size_vec: Vector2
 
 func _ready():
 	var display_width = self.size.x
 	var display_height = self.size.y
-	var size_vec := fow_scale_factor * Vector2(display_width, display_height)
+	size_vec = fow_scale_factor * Vector2(display_width, display_height)
+	print(size_vec)
 
 	# set Viewports and TextureRects to window size
 	light_sv.set_size(size_vec)
@@ -42,14 +44,15 @@ func _ready():
 
 	# add a copy of the light to the light subviewport
 	light_dup = light.duplicate()
-	#light_dup.position *= fow_scale_factor
+	light_dup.position *= fow_scale_factor
 	light_dup.apply_scale(fow_scale_factor * Vector2(1.,1.))
 	light_sv.add_child(light_dup)
 
 	# add copies of the light occluders to the light subviewport
-	for occluder in light_occluders.get_children():
+	for occluder in get_tree().get_nodes_in_group(light_occluder_group):
 		if occluder is LightOccluder2D:
-			var occluder_dup:= occluder.duplicate()
+			var occluder_dup: LightOccluder2D = occluder.duplicate()
+			occluder_dup.position *= fow_scale_factor
 			occluder_dup.apply_scale(fow_scale_factor * Vector2(1., 1.))
 			light_sv.add_child(occluder_dup)
 
@@ -60,6 +63,9 @@ func _ready():
 	material.set_shader_parameter('mask_texture', mask_texture)
 	if persistent_reveal:
 		mask.material.set_shader_parameter('mask_texture', mask_texture)
+		
+func _process(_delta):
+	light_sv.get_texture().get_image().save_png('res://test_mask.png')
 
 func on_light_moved(pos: Vector2):
 	light_dup.position = fow_scale_factor * pos
