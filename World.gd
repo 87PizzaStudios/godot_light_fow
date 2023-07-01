4fd7856fac7a8d9576e1384e20c1d67f17dfaf35
Hudson <dhudsmith@gmail.com>
2023-06-30 21:23:11 -0400

    half-working downscale feature

--- World.gd
+++ World.gd
@@ -7,5 +7,6 @@ signal on_light_moved(position: Vector2)
 func _process(_delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var pos = get_local_mouse_position()
+#		print(pos)
 		light.position = pos
 		on_light_moved.emit(pos)
